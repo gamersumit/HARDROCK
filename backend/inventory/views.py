83d3e3875d39c52b304cfc6ddb5964f4b47e2f92
Sumit Aggarwal <156279156+gamersumit@users.noreply.github.com>
2024-02-10 22:38:38 +0530

    update views.py

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 from account.mixins import AdminUserPermissionsMixin
 from .models import Inventory
@@ -6,60 +5,49 @@ from .serializers import InventorySerializer
 from rest_framework.response import Response
 import base64
 from rest_framework import permissions
-from rest_framework.pagination import LimitOffsetPagination
 # Create your views here.
 
 class InventoryDetailAPIView(generics.RetrieveAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-class InventoryAddItemView(generics.GenericAPIView):
+class InventoryAddItemView(AdminUserPermissionsMixin, generics.GenericAPIView):
+    queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
-
     def post(self, request):
         # create a user
-        try :
+        try:
+            print(request.data)
             serializer = self.serializer_class(data = request.data)
             serializer.is_valid(raise_exception = True)
-            item = serializer.save()
-            # return Response({'status': True, 'message' : 'Item added Successfully'}, status =200)
+            print(serializer.validated_data)
+            serializer.save()
+            return Response({'status': True, 'message' : 'Item added Successfully'}, status =200)
+        
         except Exception as e:
             return Response({'status': False, 'message': str(e)}, status = 400)
-
-        try :  
-            img = request.POST.get('img_source')
-            format, imgstr = img.split(';base64,')
-            base64.b64decode(imgstr)
-            item.img_source = request.data
-            item.save()
-            return Response({'status': True, 'message' : 'Registration Successful'}, status =200)
-        except:
-            return Response({'status': True, 'message' : 'Registration Successful'}, status =200)
-        
 class InventoryListAPIView(generics.ListAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
-    pagination_class = LimitOffsetPagination
-
+    permission_classes = [permissions.IsAuthenticated]
 class InventoryBreakfastListAPIView(generics.ListAPIView):
     queryset = Inventory.objects.filter(category = 'breakfast')
     serializer_class = InventorySerializer
-    pagination_class = LimitOffsetPagination
-
+    permission_classes = [permissions.IsAuthenticated]
 class InventoryLunchListAPIView(generics.ListAPIView):
     queryset = Inventory.objects.filter(category = 'lunch')
     serializer_class = InventorySerializer
-    pagination_class = LimitOffsetPagination
-
+    permission_classes = [permissions.IsAuthenticated]
 class InventoryShakesListAPIView(generics.ListAPIView):
     queryset = Inventory.objects.filter(category = 'shakes')
     serializer_class = InventorySerializer
-    pagination_class = LimitOffsetPagination
-class InventoryUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+class InventoryUpdateAPIView(AdminUserPermissionsMixin, generics.UpdateAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
 
-class InventoryDeleteAPIView(generics.DestroyAPIView):
+class InventoryDeleteAPIView(AdminUserPermissionsMixin, generics.DestroyAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
 
